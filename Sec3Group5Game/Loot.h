c4a0a2773379c974bfa080379bf715fdc36b9272
pankocatt <dinalailamak@gmail.com>
2024-03-15 14:03:26 -0400

    Created structs for Loot.h

--- Sec3Group5Game/Loot.h
+++ Sec3Group5Game/Loot.h
@@ -1,6 +1,6 @@
 #pragma once
 
-//each attribute of the structs 
+//each attribute of the structs will add to player statistic
 typedef struct sword {
 	int dmg;
 } SWORD;
@@ -13,7 +13,32 @@ typedef struct healthPot {
 	int health;
 } HEALTHPOT;
 
-//typedef struct lootPool {
-//
-//} LOOTPOOL;
+typedef enum type { NONE = 0, SWORD_TYPE, ARMOUR_TYPE, HEALTHPOT_TYPE } TYPE;
+
+typedef union loot {
+	SWORD sword;
+	ARMOUR armour;
+	HEALTHPOT healthpot;
+} LOOT;
+
+typedef struct item {
+	TYPE lootType;
+	LOOT loot;
+} ITEM;
+
+typedef struct lootPool {
+	ITEM* pool[10];
+} LOOTPOOL;
+
+ITEM CreateItemFromSword(SWORD s);
+ITEM CreateItemFromArmour(ARMOUR a);
+ITEM CreateItemFromHealthPot(HEALTHPOT p);
+
+LOOTPOOL* setUpLootPool(char* argv); //{  //set up loot pool for internal handling
+//	lootpool lp
+//	read file
+//	choose random items
+//}
+
+ITEM returnItem(LOOTPOOL* lp); //Passed to kyle, should randomly pull item from lootpool and give to him for pickup
 
