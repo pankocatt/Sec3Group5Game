6960681eaf3ab4ebf4021b08efe7cb60ca9d477b
pankocatt <dinalailamak@gmail.com>
2024-03-15 13:08:45 -0400

    Created structs for Loot.h

--- Sec3Group5Game/Loot.h
+++ Sec3Group5Game/Loot.h
@@ -1,1 +1,19 @@
 #pragma once
+
+//each attribute of the structs 
+typedef struct sword {
+	int dmg;
+} SWORD;
+
+typedef struct armour {
+	int def;
+} ARMOUR;
+
+typedef struct healthPot {
+	int health;
+} HEALTHPOT;
+
+//typedef struct lootPool {
+//
+//} LOOTPOOL;
+
