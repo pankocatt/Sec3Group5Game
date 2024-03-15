566d7436a005609e767c25adae3bfd5f1f3acc0a
PersonalHuntRhys <Rhunt2963@conestogac.on.ca>
2024-03-15 13:05:38 -0400

    Added function declarations to Player.h

--- Sec3Group5Game/Player.h
+++ Sec3Group5Game/Player.h
@@ -1,1 +1,19 @@
 #pragma once
+typedef struct player {
+	int health;
+	int damage;
+	int defence;
+	//int critChance;
+	int healthPots;
+	char* playerName; 
+}Player;
+
+void setName(char* name);
+
+void equipWeapon(int dmg);
+
+void equipArmor(int dfc);
+
+void addHealthPot(int healthPot);
+
+void playerTakeDmg(int dmg);
